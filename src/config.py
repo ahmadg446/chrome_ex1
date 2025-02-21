082fa7d7b4181d8a479943c75e310d6ebea7f67d
Ahmad A Ghunaim <ahmadtheg06@gmail.com>
2025-02-21 22:35:49 +0000

    v1

--- src/config.py
+++
@@ -1,8 +0,0 @@
-import os
-PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-CONFIG = {
-    "log_file" : os.path.join(PROJECT_ROOT, "logs/process.log"),
-    "log_level": "INFO",
-    "src_directory": os.path.join(PROJECT_ROOT, "src"),
-}
